0994fca1c348c1fea5e83ecc8aeb1a839a98b7b3
Jens De Craecker <jens.decraecker@icapps.com>
2019-12-18 12:37:38 +0100

    add title component

---
+++ src/components/title/Title.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { GENERAL } from '../../utils/_colors';
+
+export const Title = styled.h2`
+  font-size: 16px;
+  color: ${GENERAL.YELLOW};
+  text-align: center;
+  margin-bottom: 5px;
+  text-transform: uppercase;
+`;
